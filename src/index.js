cd55c47eb61d7170f9ec288b2def1cb19ba011a8
KhaleaM <106846809+KhaleaM@users.noreply.github.com>
2026-04-01 16:18:58 -0500

    updated index.html index.js style.css

--- src/index.js
+++ src/index.js
@@ -35,27 +35,27 @@ function formatDate(date) {
 
     if (minutes < 10) {
         minutes = `0${minutes}`;
-    }
+    } 
 
     return `${day} ${hours}:${minutes}`;
-};
+}
 
 
 
 function searchCity(city) {
     let apiKey = "e2t5dd99b4cfa003ed9aaof63c44d73c";
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
     axios.get(apiUrl).then(refreshWeather);
 }
 
 function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
-    
+
     searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", handleSearchSubmit);
+searchFormElement.addEventListener("click", handleSearchSubmit);
 
-searchCity("Paris")
+searchCity("Paris");
